e274a815c689f838ec703477c41e6e989754700b
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-07 21:53:20 -0300

    Add files via upload

---
+++ Engenharia de Software/Algoritmos - Aula 4/TresNumerosZero.py
@@ -0,0 +1,8 @@
+n1 = float(input('=> Digite um valor qualquer: '))
+n2 = float(input('=> Digite outro valor qualquer: '))
+n3 = float(input('=> Digite outro valor qualquer: '))
+print('------------------')
+
+if n1 != 0 and n2 != 0 and n3 != 0:
+    print('=> Todos os números são diferentes de zero!')
+elif (n1 != 0 or n2 != 0)
