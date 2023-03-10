300e5b835fdbbdf8bdea74ff9d0986c9980835a5
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-10 11:20:08 -0300

    Delete question3.py

--- Engenharia de Software/Algoritmos - Aula 1/question3.py
+++
@@ -1,6 +0,0 @@
-print('=! Welcome to the restaurant TUDO GOSTOSO!')
-weight = float(input('=? What is the weight of the meal in KG? '))
-print('')
-
-price = weight * 30
-print(f'=> The price of the meal with {weight} Kg is: {price}.')
