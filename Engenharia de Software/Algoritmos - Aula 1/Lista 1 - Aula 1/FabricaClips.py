ba41c7edeacc97ad1a4d76a02cd3dd3614153717
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-10 09:42:45 -0300

    Update Algoritmos - Aula 1

---
+++ Engenharia de Software/Algoritmos - Aula 1/Lista 1 - Aula 1/FabricaClips.py
@@ -0,0 +1,9 @@
+pbox = int(input('=? How many boxes of plastic clips did you sell? '))
+mbox = int(input('=? And how many boxes of metal clips did yoy sell? '))
+print('')
+
+ptotal = pbox * 5
+mtotal = mbox * 10
+
+print(f'=> Amount of the plastic boxes: ${ptotal} \n'
+      f'=> Amount of the metal boxes: ${mtotal}')
