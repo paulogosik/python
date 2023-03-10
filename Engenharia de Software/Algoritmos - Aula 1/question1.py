c8663850b5b079957d4d90059e146642357d9c61
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-10 11:18:31 -0300

    Delete question1.py

--- Engenharia de Software/Algoritmos - Aula 1/question1.py
+++
@@ -1,7 +0,0 @@
-name = input('=? Olá, aluno, qual o seu nome? ')
-n1 = float(input(f'=? {name}, qual é a sua primeira nota? '))
-n2 = float(input('=? E a sua segunda nota? '))
-
-media = (n1 + n2) / 2
-
-print(f'{name} obteve a média {media}!')
