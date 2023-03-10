8b3a7a80b10553763f2b5b2c7ff20111f1c1a01a
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-10 11:17:53 -0300

    Delete Engenharia de Software/Algoritmos - Aula 1/Lista 1 - Aula 1 directory

--- Engenharia de Software/Algoritmos - Aula 1/Lista 1 - Aula 1/RacionamentoEnergia.py
+++
@@ -1,8 +0,0 @@
-salary = float(input('=? Hello, what is your salary? '))
-quant = float(input('=? And how much energy you spend in your house in kilowatts? '))
-print('')
-
-cent = salary * 0.01
-bill = cent * quant
-
-print(f'=! Your salary is ${salary}, so, your bill will be {bill}!')
