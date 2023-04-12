b2293bdb176aea51254367073d73e23536199c86
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-04-12 16:24:01 -0300

    Create SalarioFabrica.py

---
+++ Engenharia de Software/Algoritmos - Aula 8/Lista 10/SalarioFabrica.py
@@ -0,0 +1,12 @@
+# salarioMinimo = float(input("=> Informe o valor do salário mínimo: "))
+
+# nome = input("=> Informe o nome do operário: ")
+# nome = nome.capitalize()
+
+# while nome != "Sair":
+    
+    
+#     nome = input("=> Informe o nome do operário: ")
+#     nome = nome.capitalize()
+
+print("=> Em andamento!")
