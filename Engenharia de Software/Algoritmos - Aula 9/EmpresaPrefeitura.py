5aed84f9d3d56a0b299c9bbf1dea07e6bf6bd8bb
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-04-26 17:32:12 -0300

    Update EmpresaPrefeitura.py

---
+++ Engenharia de Software/Algoritmos - Aula 9/EmpresaPrefeitura.py
@@ -0,0 +1,10 @@
+metragem = 1
+
+valorMinimo = float(input("=> Informe o valor mínimo: "))
+
+while metragem != 0:
+    metragem = float(input("=> Informe a metragem da casa (m²): "))
+    classe = input("=> Informe a classe da casa (A ou B): ")
+    classe = classe.upper()
+    
+    
