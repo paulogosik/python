a358fe6a64dfc6324b8c80ecce557e7b691a4c69
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-05-29 17:31:30 -0300

    Create InformacoesCidades.py

---
+++ Engenharia de Software/Algoritmos - Aula 12/Lista 13/InformacoesCidades.py
@@ -0,0 +1,29 @@
+nomes = []
+habitantes = []
+superficies = []
+i = 0
+
+while i < 3:
+    nome = input(f"=> {i + 1} - Informe o nome da cidade: ")
+    populacao = int(input(f"=> {i + 1} - Informe a quantidade de habitantes da cidade: "))
+    superficie = float(input(f"=> {i + 1} - Informe a superfície da cidade (em metros quadrados): "))
+    
+    nomes.append(nome)
+    habitantes.append(populacao)
+    superficies.append(superficie)
+    
+    print("--------------------------")
+    
+    i += 1
+    
+print(nomes)
+print(habitantes)
+print(superficies)
+
+i = 0
+while i < len(nomes):
+    print("--------------------------")
+    densidade = habitantes[i]/superficies[i]
+    print(f"=> {nomes[i]} - {densidade} pessoas/m²")
+    
+    i += 1
