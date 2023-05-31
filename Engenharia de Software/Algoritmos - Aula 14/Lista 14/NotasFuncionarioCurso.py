2a6058826a4cf79ed8220205b6fb18f470d158c4
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-05-31 17:34:01 -0300

    Update NotasFuncionarioCurso.py

--- Engenharia de Software/Algoritmos - Aula 14/Lista 14/NotasFuncionarioCurso.py
+++ Engenharia de Software/Algoritmos - Aula 14/Lista 14/NotasFuncionarioCurso.py
@@ -6,8 +6,8 @@ while i < 3:
     funcionario = []
     funcionario.append(input(f"=> Informe o nome do funcionário {i + 1}: "))
     funcionario.append(input(f"=> Informe o setor do funcionário {i + 1}: "))
-    funcionario.append(input(f"=> Informe o nota escrita do funcionário {i + 1}: "))
-    funcionario.append(input(f"=> Informe o nota prática do funcionário {i + 1}: "))
+    funcionario.append(float(input(f"=> Informe o nota escrita do funcionário {i + 1}: ")))
+    funcionario.append(float(input(f"=> Informe o nota prática do funcionário {i + 1}: ")))
     print("----------------------------")
     
     listaFuncionarios.append(funcionario)
@@ -16,6 +16,15 @@ while i < 3:
     
 i = 0
 while i < len(listaFuncionarios):
-    print(f"=> Funcionário: {listaFuncionarios[]}")
+    print(f"=> Funcionário: {listaFuncionarios[i][0]} - Setor: {listaFuncionarios[i][1]} - Nota escrita: {listaFuncionarios[i][2]} - Nota prática: {listaFuncionarios[i][3]}")
+    
+    i += 1
+print("----------------------------")
+
+print(f"=> Funcionários aprovados: ")
+i = 0
+while i < len(listaFuncionarios):
+    if (listaFuncionarios[i][2] + listaFuncionarios[i][3]) >= 7:
+        print(f"=> {listaFuncionarios[i][0]}")
     
     i += 1
