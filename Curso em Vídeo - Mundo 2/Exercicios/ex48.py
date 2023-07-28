5ddf7a587b575c99e343fa718ab5ecf0dd6af70b
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-07-28 10:53:57 -0300

    Update ex48.py

--- Curso em Vídeo - Mundo 2/Exercicios/ex48.py
+++ Curso em Vídeo - Mundo 2/Exercicios/ex48.py
@@ -1,4 +1,7 @@
 sum = 0
 
 for c in range(1, 500):
-    print(f"{c}")
+    if (c % 2 != 0) and (c % 3 == 0):
+        sum += c
+
+print(f"=> The sum of all the odd divisible by three numbers is: {sum}")
