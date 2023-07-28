281af89dedcb8e31f45df17003b65f915e3b6187
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-07-28 15:51:52 -0300

    Update exercicios-python

--- Curso em Vídeo - Mundo 2/Exercicios/ex50.py
+++ Curso em Vídeo - Mundo 2/Exercicios/ex50.py
@@ -4,3 +4,6 @@ for c in range(0, 6):
     num = int(input(f"=> Inform any integer number: "))
     if num % 2 == 0:
         sum += num
+
+print("------------------------------")
+print(f"=> The sum of the even numbers equals: {sum}")
