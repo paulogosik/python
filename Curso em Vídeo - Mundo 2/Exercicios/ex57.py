0114af638f647ea846fb527cfb8878be34cfa4bb
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-07-31 11:15:58 -0300

    Create ex57.py

---
+++ Curso em Vídeo - Mundo 2/Exercicios/ex57.py
@@ -0,0 +1,7 @@
+sex = input(f"=> Type your sex [M/F]: ").upper()
+
+while (sex != "M") and (sex != "F"):
+    sex = input(f"=> Type your sex [M/F]: ").upper()
+
+print("------------")
+print(f"=> OK, thanks!")
