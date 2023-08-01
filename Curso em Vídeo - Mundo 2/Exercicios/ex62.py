39ef0d0da5844c346aa90349ad85334831aa8090
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-01 15:08:55 -0300

    Update ex62.py

--- Curso em Vídeo - Mundo 2/Exercicios/ex62.py
+++ Curso em Vídeo - Mundo 2/Exercicios/ex62.py
@@ -1,13 +1,13 @@
 firstTerm = int(input("=> Inform randomly a first term: "))
 ratio = int(input("=> Now, inform the ratio: "))
-terms = int(input("=> How much terms do you wanna see? "))
 pa = firstTerm
+terms = 10
 i = 0
 
-while i <= terms:
-    if i == 0:
-        print(firstTerm)
-    else:
+while terms != 0:
+    i = 0
+    while i <= terms:
         print(pa)
-    pa += ratio
-    i += 1
+        pa += ratio
+        i += 1
+    terms = int(input("=> How much terms do you wanna see more? "))
