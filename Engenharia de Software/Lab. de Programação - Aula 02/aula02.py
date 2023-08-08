a4d2659dbc8eb1d2068a591056b272f4375be22b
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-07 21:23:15 -0300

    Update aula02.py

--- Engenharia de Software/Lab. de Programação - Aula 02/aula02.py
+++ Engenharia de Software/Lab. de Programação - Aula 02/aula02.py
@@ -1,9 +1,10 @@
-import calcula
+from calcula import *
+# import calcula
 # from calcula import soma, mult
 
-print(calcula.soma(6, 2))
-# print(soma(6, 2))
-print(calcula.sub(6, 2))
-print(calcula.mult(6, 2))
-# print(mult(6, 2))
-print(calcula.div(6, 2))
+# print(calcula.soma(6, 2))
+print(soma(6, 2))
+print(sub(6, 2))
+# print(calcula.mult(6, 2))
+print(mult(6, 2))
+print(div(6, 2))
