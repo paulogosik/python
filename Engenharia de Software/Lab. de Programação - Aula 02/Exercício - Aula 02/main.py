989e7b6ca2eccc776dc54ee470c8f02e89975fd7
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-09 09:55:40 -0300

    Update main.py

--- Engenharia de Software/Lab. de Programação - Aula 02/Exercício - Aula 02/main.py
+++ Engenharia de Software/Lab. de Programação - Aula 02/Exercício - Aula 02/main.py
@@ -1,10 +1,10 @@
-from funcaoEx01 import *
-from funcaoEx02 import *
-from funcaoDoisValores import *
-from funcaoIdade import *
-from funcaoNumerosCrescentes import *
-from funcaoSegundosEmHoras import *
-from funcaoTresNotas import *
+from funcaoEx01 import * # type: ignore
+from funcaoEx02 import * # type: ignore
+from funcaoDoisValores import * # type: ignore
+from funcaoIdade import * # type: ignore
+from funcaoNumerosCrescentes import * # type: ignore
+from funcaoSegundosEmHoras import * # type: ignore
+from funcaoTresNotas import * # type: ignore
 
 opc = ""
 while opc != "sair":
@@ -22,24 +22,24 @@ while opc != "sair":
     print("-------------------")
     if opc == "1": # OPÇÃO 01 --------------------------------------------------------------------
         # Exercício 01
-        verif = VerificaPositivo1(0)
+        verif = VerificaPositivo1(0) # type: ignore
         print(verif)
         
     elif opc == "2": # OPÇÃO 02 --------------------------------------------------------------------
         # Exercício 02
-        verif = VerificaPositivo2(-4)
+        verif = VerificaPositivo2(-4) # type: ignore
         print(f"O valor N é {verif}")
         
     elif opc == "3":# OPÇÃO 03 --------------------------------------------------------------------
         num = int(input(f"=> Informe um valor inteiro: "))
-        par_ou_impar(num)
+        par_ou_impar(num) # type: ignore
         
     elif opc == "4": # OPÇÃO 04 --------------------------------------------------------------------
         anos = int(input(f"=> Informe quantos anos você tem: "))
         meses = int(input(f"=> Tirando os anos, quantos meses: "))
         dias = int(input("=> Tirando os meses, quantos dias: "))
 
-        idadeDias = IdadeEmDias(anos, meses, dias)
+        idadeDias = IdadeEmDias(anos, meses, dias) # type: ignore
         print(f"=> Você tem {idadeDias} dias de idade")
     
     elif opc == "5": # OPÇÃO 05 --------------------------------------------------------------------
@@ -47,13 +47,12 @@ while opc != "sair":
         n2 = int(input("=> Informe outro número inteiro: "))
         n3 = int(input("=> Informe mais um número inteiro: "))
 
-        ordem = OrdemCrescente(n1, n2, n3)
-
+        ordem = OrdemCrescente(n1, n2, n3) # type: ignore
         print(f"=> A ordem crescente é: {ordem}")
         
     elif opc == "6": # OPÇÃO 06 --------------------------------------------------------------------
         segundos = int(input(f"=> Informe o tempo representado em segundos: "))
-        tempo = segundos_em_horas(segundos)
+        tempo = segundos_em_horas(segundos) # type: ignore
         
     elif opc == "7": # OPÇÃO 07 --------------------------------------------------------------------
         tipo = input("=> Informe o tipo da matéria: ").upper()
@@ -61,7 +60,7 @@ while opc != "sair":
         n2 = float(input(f"=> Informe a N2: "))
         n3 = float(input(f"=> Informe a N3: "))
 
-        media = CalcularMedia(n1, n2, n3, tipo)
+        media = CalcularMedia(n1, n2, n3, tipo) # type: ignore
 
         print(f"=> A média do aluno foi: {media}")
     
