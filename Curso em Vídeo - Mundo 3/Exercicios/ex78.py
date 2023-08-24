9e9b6b1b01220921a27648a51c55276629a41e80
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-24 10:52:19 -0300

    Update ex78.py

--- Curso em Vídeo - Mundo 3/Exercicios/ex78.py
+++ Curso em Vídeo - Mundo 3/Exercicios/ex78.py
@@ -1,6 +1,6 @@
 nums = []
-lowerNum = 0
-higherNum = 0
+lowerNum = []
+higherNum = []
 
 for c in range(5):
     nums.append(int(input(f"=> Inform any integer number: ")))    
@@ -8,12 +8,16 @@ print(f"-" * 45)
 
 for i, num in enumerate(nums):
     if i == 0:
-        lowerNum = num
-        higherNum = num
-    if num < lowerNum:
-        lowerNum = num
-    elif num > higherNum:
-        higherNum = num
+        lowerNum.append(i)
+        lowerNum.append(num)
+        higherNum.append(i)
+        higherNum.append(num)
+    if num < lowerNum[1]:
+        lowerNum[0] = i
+        lowerNum[1] = num
+    elif num > higherNum[1]:
+        higherNum[0] = i
+        higherNum[1] = num
 
-print(f"=> The lowest number was: {lowerNum}")
-print(f"=> The highest number was: {higherNum}")
+print(f"=> The lowest number was {lowerNum[1]} in the {lowerNum[0] + 1}° place")
+print(f"=> The highest number was: {higherNum[1]} in the {higherNum[0] + 1}° place")
