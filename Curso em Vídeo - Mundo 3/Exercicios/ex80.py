b426716b979f0ff7c5763e663fd84a8c76252089
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-25 15:00:41 -0300

    Update ex80.py

--- Curso em Vídeo - Mundo 3/Exercicios/ex80.py
+++ Curso em Vídeo - Mundo 3/Exercicios/ex80.py
@@ -1,1 +1,21 @@
-list
+list = []
+
+for c in range(5):
+    num = int(input(f"=> Inform any integer number: "))
+    
+    if (c == 0) or (num > list[len(list) - 1]):
+        list.append(num)
+        print(f"=> The number {num} was added at the final")
+    elif num < list[0]:
+        list.insert(0, num)
+        print(f"=> The number {num} was added at the posiniton number 0")
+    else:
+        pos = 0
+        while pos < len(list):
+            if num <= list[pos]:
+                list.insert(pos, num)
+                print(f"=> The number {num} was added at the position number {pos}")
+                break
+            pos += 1    
+print(f"-" * 45)
+print(f"=> The list ordened is: {list}")
