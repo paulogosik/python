3344e926c1150891d374b3977dc9c34f5faf5823
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-09-05 17:37:41 -0300

    Create Ex04_UltimaPalavra

---
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex04_UltimaPalavra/funcao.py
@@ -0,0 +1,5 @@
+def ultima(frase):
+    palavras = frase.split(" ")
+    ultimaPalavra = palavras[-1]
+    
+    return ultimaPalavra
---
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex04_UltimaPalavra/main.py
@@ -0,0 +1,5 @@
+import funcao # type: ignore
+
+frase = input("=> Informe sua frase: ")
+ultimaPalavra = funcao.ultima(frase)
+print(f"=> A última palavra é: {ultimaPalavra}")
