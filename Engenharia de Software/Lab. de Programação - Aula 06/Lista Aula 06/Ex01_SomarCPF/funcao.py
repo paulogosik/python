a04f9c5e2a348b374d6cb29242575dc599f087a0
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-09-04 21:42:54 -0300

    Update python

--- Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex01_SomarCPF/funcao.py
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex01_SomarCPF/funcao.py
@@ -1,5 +1,16 @@
 def somar_cpf(cpf):
     somaTotal = 0
-    lista 
+    
+    lista = cpf.split(".")
+    parteFinal = lista[2].split("-")
+    del lista[2]
+    
+    for item in parteFinal:
+        lista.append(item)
+    
+    for item in lista:
+        somaTotal += int(item)
+    
+    print(lista)
     
     return somaTotal
--- Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex01_SomarCPF/main.py
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex01_SomarCPF/main.py
@@ -1,6 +1,6 @@
 import funcao
 
-cpf = input(f"=> Informe seu CPF: ")
+cpf = input(f"=> Informe seu CPF (siga o exemplo 111.222.333-44): ")
 somaTotal = funcao.somar_cpf(cpf)
 
 print(f"=> A soma do número [{cpf}] é: {somaTotal}")
