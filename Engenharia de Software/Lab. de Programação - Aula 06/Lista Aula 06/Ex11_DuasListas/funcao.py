64227011329d1f56d0f02ff610157718c8a01f96
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-09-11 19:47:36 -0300

    Create Ex11

---
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex11_DuasListas/funcao.py
@@ -0,0 +1,3 @@
+def juntar_com_virgula(frase1, frase2):
+    frasesConcatenadas = f"{frase1}, {frase2}"
+    return frasesConcatenadas
---
+++ Engenharia de Software/Lab. de Programação - Aula 06/Lista Aula 06/Ex11_DuasListas/main.py
@@ -0,0 +1,6 @@
+import funcao
+
+frase1 = input("=> Digite a frase 1: ")
+frase2 = input("=> Digite a frase 2: ")
+frasesConcatenadas = funcao.juntar_com_virgula(frase1, frase2)
+print(f"=> As frases concatenadas ficaram: {frasesConcatenadas}")
