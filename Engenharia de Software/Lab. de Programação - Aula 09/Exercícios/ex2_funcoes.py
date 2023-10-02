e80fd4912f5608fe1a20f5e4b330a74352156d23
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-10-02 16:42:23 -0300

    update python

--- Engenharia de Software/Lab. de Programação - Aula 09/Exercícios/ex2_funcoes.py
+++ Engenharia de Software/Lab. de Programação - Aula 09/Exercícios/ex2_funcoes.py
@@ -1,5 +1,39 @@
 def naPosicao(palavra, pos):
-    if pos > len(palavra):
+    if pos >= len(palavra):
         return -1
     else:
         return palavra[pos]
+
+
+def posicoes(palavra, letra):
+    for pos, let in enumerate(palavra):
+        if let == letra:
+            print(pos)
+
+
+def iniciaCom(palavra, letra):
+    if palavra[0] == letra:
+        return True
+    else:
+        return False
+
+
+def maiorPalavra(lista):
+    maiorletras = 0
+    maiorpalavra = None
+    for palavra in lista:
+        atualpalavra = 0
+        for letra in palavra:
+            atualpalavra += 1
+        if atualpalavra > maiorletras:
+            maiorpalavra = palavra
+
+    return maiorpalavra
+
+
+def ehConector(palavra):
+    conectores = ["da", "de", "das", "do", "dos", "e"]
+    if palavra in conectores:
+        return True
+    else:
+        return False
--- Engenharia de Software/Lab. de Programação - Aula 09/Exercícios/ex2_palavras.py
+++ Engenharia de Software/Lab. de Programação - Aula 09/Exercícios/ex2_palavras.py
@@ -1,2 +1,31 @@
 from ex2_funcoes import *
 
+palavra = input("Informe uma palavra: ").title()
+print("-" * 30)
+
+# Questão A)
+pos = 2
+print(f"A letra na {pos}° posição da palavra [{palavra}] é: {naPosicao(palavra, pos)}")
+pos = 6
+print(f"A letra na {pos}° posição da palavra [{palavra}] é: {naPosicao(palavra, pos)}")
+print("-" * 30)
+
+# Questão B)
+print(f"As posições de [a] na palavra [{palavra}]:")
+posicoes(palavra, "a")
+print("-" * 30)
+
+# Questão C)
+print(f"A palavra [{palavra}] começa com R? {iniciaCom(palavra, 'R')}")
+print(f"A palavra [{palavra}] começa com C? {iniciaCom(palavra, 'C')}")
+print("-" * 30)
+
+# Questão D)
+lista = ["Cavalo", "Teste", "Lagarto", "Fazenda"]
+print(lista)
+print(f"=> A maior palavra da lista é: {maiorPalavra(lista)}")
+print("-" * 30)
+
+# Questão E)
+print(f"A palavra [{palavra}] é um conector? {ehConector(palavra)}")
+print(f"A palavra [dos] é um conector? {ehConector('dos')}")
