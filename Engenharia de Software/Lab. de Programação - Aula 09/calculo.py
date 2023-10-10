ba522b76bf2149986b57a8afd21b7e1013e58026
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-10-10 08:38:54 -0300

    update calculo.py

--- Engenharia de Software/Lab. de Programação - Aula 09/calculo.py
+++ Engenharia de Software/Lab. de Programação - Aula 09/calculo.py
@@ -13,7 +13,7 @@ def areaCirculo(r):
     return pi * r ** 2
 
 
-def areaQuadrado(ld1, ld2 = None):
+def areaQuadrado(ld1, ld2=None):
     if ld1 <= 0 or ld2 <= 0:
         return -1
     if ld2 is None:
