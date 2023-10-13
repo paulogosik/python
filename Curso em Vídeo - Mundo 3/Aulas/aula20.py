39686bfa15cd81755141bb6a9dcfccf9e69b78de
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-10-13 16:35:14 -0300

    Update aula20.py

--- Curso em Vídeo - Mundo 3/Aulas/aula20.py
+++ Curso em Vídeo - Mundo 3/Aulas/aula20.py
@@ -1,9 +1,33 @@
-def message(msg):
+def l():
     print("-" * 30)
+
+
+def message(msg):
     print(f"{msg:^30}")
     print("-" * 30)
+
+
+def sum_ab(a, b):
+    return a + b
+
+
+def counter(*num):
+    if len(num) == 0:
+        return "Empty elements"
+    else:
+        return len(num)
 
 
 message("Hello, World!")
 message("I am Paulo")
 message("ur creator")
+
+print(sum_ab(2, 3))
+print(sum_ab(5, 6))
+print(sum_ab(1, 2))
+
+l()
+print(counter())
+print(counter(1, 2, 3, 4, 5))
+print(counter(6, 7, 8, 9))
+
