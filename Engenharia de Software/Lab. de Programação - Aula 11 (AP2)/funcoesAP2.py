1b93e9610c4c57781cbafc16061e30f56a7375b5
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-10-16 19:54:06 -0300

    Update funcoesAP2.py

--- Engenharia de Software/Lab. de Programação - Aula 11 (AP2)/funcoesAP2.py
+++ Engenharia de Software/Lab. de Programação - Aula 11 (AP2)/funcoesAP2.py
@@ -2,31 +2,33 @@ def maiorPalavra(frase):
     palavras = frase.split(" ")
     palavraMaior = 0
     maioresPalavras = []
-    
+
     for palavra in palavras:
         letras = len(palavra)
         if letras > palavraMaior:
             palavraMaior = letras
-    
+
     for palavra in palavras:
         if len(palavra) == palavraMaior:
             maioresPalavras.append(palavra)
-        
+
     if len(maioresPalavras) == 1:
         return ''.join(maioresPalavras)
     else:
         return maioresPalavras
-    
+
+
 def corrigeMN(frase):
     palavras = frase.split(" ")
     palavras_corrigidas = []
-    
+
     for palavra in palavras:
         palavra = palavra.replace("np", "mp")
         palavra = palavra.replace("nb", "mb")
         palavras_corrigidas.append(palavra)
-    
+
     return ' '.join(palavras_corrigidas)
+
 
 def contaTexto(frase):
     palavras = frase.split(" ")
@@ -40,9 +42,10 @@ def contaTexto(frase):
             listaPalavra.append(palavras.count(palavra))
             contaPalavras.append(listaPalavra[:])
             listaPalavra.clear()
-        
+
     return contaPalavras
-        
+
+
 def verificaPalindromo(palavra):
     palavraNormal = []
     palavraReverse = []
@@ -50,7 +53,7 @@ def verificaPalindromo(palavra):
         palavraNormal.append(letra)
     palavraReverse = palavraNormal[:]
     palavraReverse.reverse()
-    
+
     if palavraReverse == palavraNormal:
         return True
     else:
