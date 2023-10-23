daa78314fc6b8a7a199203c600c4d928817bd4d2
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-10-23 16:40:19 -0300

    update python

--- Engenharia de Software/Lab. de Programação - Aula 12/Lista de Exercícios/Ex01_Login.py
+++ Engenharia de Software/Lab. de Programação - Aula 12/Lista de Exercícios/Ex01_Login.py
@@ -1,13 +1,52 @@
+from Ex01_funcoes import *
+
 usuarios = dict()
 usuarios['mickey'] = {"Login": "mickey", "Nome": "Mickey Mouse", "Último acesso": "22/06/2021", "Máquina": "larc01"}
 usuarios['minnie'] = {"Login": "minnie", "Nome": "Minerva Mouse", "Último acesso": "21/06/2021", "Máquina": "lab15"}
 
 while True:
-    print(f"Escolha uma das opções abaixo:\n"
-          "\t[1] Inserir usuário\n"
-          "\t[2] Listar os nomes dos usuários\n"
-          "\t[3] Listar dados de um usuário\n"
-          "\t[4] Listar usuários com último acesso específico\n"
-          "\t[5] \n"
-          "\t[6] Inserir usuário\n"
-          "\t[999] Sair")
+      print(f"Escolha uma das opções abaixo:\n"
+            "\t[1] Inserir usuário\n"
+            "\t[2] Listar os nomes dos usuários\n"
+            "\t[3] Listar dados de um usuário\n"
+            "\t[4] Listar usuários com último acesso específico\n"
+            "\t[5] Alterar dados de um usuário\n"
+            "\t[6] Excluir um usuário\n"
+            "\t[0] Sair")
+      opc = int(input(">>> "))
+      print("-" * 30)
+
+      if opc == 1:
+            login_novo_usuario, novo_usuario = inserir_usuario()
+            usuarios[login_novo_usuario] = novo_usuario
+            print("-" * 15)
+            print(f"Usuário adicionado com sucesso!")
+            print("-" * 30)
+
+      elif opc == 2:
+            listar_nomes(usuarios)
+            print("-" * 30)
+
+      elif opc == 3:
+            listar_dados(usuarios)
+            print("-" * 30)
+
+      elif opc == 4:
+            ultimo_acesso(usuarios)
+            print("-" * 30)
+
+      elif opc == 5:
+            user = input("Informe o usuário que você deseja alterar informações: ")
+            dados_alterados = alterar_dados(usuarios.get(user).copy())
+            usuarios[user] = dados_alterados
+            print("-" * 30)
+
+      elif opc == 6:
+            user = input("Informe o usuário que você deseja excluir: ")
+            deletar_usuario(user=user, usuarios=usuarios)
+            print("-" * 15)
+            print("Usuário excluído com sucesso!")
+            print("-" * 30)
+
+      elif opc == 0:
+            break
---
+++ Engenharia de Software/Lab. de Programação - Aula 12/Lista de Exercícios/Ex01_funcoes.py
@@ -0,0 +1,73 @@
+def inserir_usuario():
+    novo_usuario = dict()
+
+    login = input("Informe o login do novo usuário: ")
+    novo_usuario["Login"] = login
+    novo_usuario["Nome"] = input("Informe o nome do novo usuário: ")
+    novo_usuario["Último acesso"] = input("Informe o último acesso: ")
+    novo_usuario["Máquina"] = input("Informe a máquina: ")
+
+    return login, novo_usuario
+
+
+def listar_nomes(usuarios):
+    for user in usuarios.values():
+        for key, value in user.items():
+            if key == "Nome":
+                print(f"{key}: {value}")
+
+
+def listar_dados(usuarios):
+    user = input("Informe um usuário para listar os dados: ")
+    print("-" * 15)
+
+    usuarios_criados = usuarios.keys()
+    if user not in usuarios_criados:
+        print("Usuário não encontrado!")
+    else:
+        user_listado = usuarios.get(user)
+        for key, value in user_listado.items():
+            print(f"{key}: {value}")
+
+
+def ultimo_acesso(usuarios):
+    acesso = input("Informe o último acesso que deseja procurar: ")
+    print("-" * 15)
+    print(f"Usuários com o último login dia {acesso}:")
+    for user in usuarios.values():
+        if user['Último acesso'] == acesso:
+            print(user['Login'])
+
+
+def alterar_dados(user):
+    print("-" * 15)
+    print("Informe o dado que você deseja alterar\n"
+          "\t[1] Todos os dados\n"
+          "\t[2] Nome\n"
+          "\t[3] Último acesso\n"
+          "\t[4] Máquina\n")
+    opc = int(input(">> "))
+    print("-" * 15)
+
+    for key in user.keys():
+        if opc == 1:
+            if key != "Login":
+                user[key] = input(f"Atualize o dado ({key}): ")
+
+        elif opc == 2:
+            if key == "Nome":
+                user[key] = input(f"Atualize o dado ({key}): ")
+
+        elif opc == 3:
+            if key == "Último acesso":
+                user[key] = input(f"Atualize o dado ({key}): ")
+
+        elif opc == 4:
+            if key == "Máquina":
+                user[key] = input(f"Atualize o dado ({key}): ")
+
+    return user
+
+
+def deletar_usuario(user, usuarios):
+    del usuarios[user]
