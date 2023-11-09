16afabb181dba34b69d01227fe690957d59a13a1
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-11-09 17:19:14 -0300

    Update ex100.py

---
+++ Curso em Vídeo - Mundo 3/Exercicios/ex100.py
@@ -0,0 +1,28 @@
+from random import randint
+from time import sleep
+
+def draw():
+    drawn_values = []
+    print("Drawing 5 values:", end=' ')
+    for i in range(5):
+        num = randint(1, 10)
+        drawn_values.append(num)
+        print(num, end=' ')
+        sleep(0.2)
+
+    print("\nDone!")
+    return drawn_values
+
+
+def sumEven(drawn_values):
+    sleep(0.5)
+    sum = 0
+    for value in drawn_values:
+        if value % 2 == 0:
+            sum += value
+
+    print(f"Adding the even numbers from {drawn_values}, we have {sum} as total.")
+
+
+drawnvalues = draw()
+sumEven(drawnvalues)
