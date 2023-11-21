7e255f764fd78526b9ff9131fd93243a64d835d7
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-11-21 11:25:11 -0300

    Update ex103.py

--- Curso em Vídeo - Mundo 3/Exercicios/ex103.py
+++ Curso em Vídeo - Mundo 3/Exercicios/ex103.py
@@ -2,15 +2,15 @@ def ficha(jogador="<desconhecido>", gols="0"):
     print(f"O jogador {jogador} fez {gols} gol(s) no campeonato.")
 
 
-jogador = input("Informe o nome do jogador: ")
-gols = input("Informe a quantidade de gols marcados: ")
+jog = input("Informe o nome do jogador: ")
+gol = input("Informe a quantidade de gols marcados: ")
 print("-" * 15)
 
-if len(jogador) == 0 and len(gols) == 0:
+if len(jog) == 0 and len(gol) == 0:
     ficha()
-elif len(jogador) == 0 and len(gols) != 0:
-    ficha(gols=gols)
-elif len(jogador) != 0 and len(gols) == 0:
-    ficha(jogador=jogador)
+elif len(jog) == 0 and len(gol) != 0:
+    ficha(gols=gol)
+elif len(jog) != 0 and len(gol) == 0:
+    ficha(jogador=jog)
 else:
-    ficha(jogador, gols)
+    ficha(jog, gol)
