0a546b5fb1a30a624f408e638705252a2e01476b
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-11-27 17:30:47 -0300

    create env ex107

---
+++ Curso em Vídeo - Mundo 3/Exercicios/ex107f.py
@@ -0,0 +1,16 @@
+def half(n):
+    return n / 2
+
+
+def double(n):
+    return n * 2
+
+
+def increase(n, percentage):
+    partN = n * (percentage / 100)
+    return n + partN
+
+
+def decrease(n, percentage):
+    partN = n * (percentage / 100)
+    return n - partN
