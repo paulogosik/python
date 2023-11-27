4be4b6a6063e472c003338c51c317069518fed28
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-11-27 17:46:02 -0300

    create pack ex108

---
+++ Curso em Vídeo - Mundo 3/Exercicios/ex108.py
@@ -0,0 +1,7 @@
+from ex108f import *
+
+num = float(input("Type any number: "))
+print(f"The half value of {money(num)} is: {money(half(num))}")
+print(f"The double value of {money(num)} is: {money(double(num))}")
+print(f"Increasing 10%, we have: {money(increase(num, 10))}")
+print(f"Decreasing 20%, we have: {money(decrease(num, 13))} ")
