c2b808811841aa50cf409708e9424b4485dd4963
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-12-18 10:21:25 -0300

    Update aula23.py

--- Curso em Vídeo - Mundo 3/Aulas/aula23.py
+++ Curso em Vídeo - Mundo 3/Aulas/aula23.py
@@ -1,7 +1,19 @@
-n = int(input("Type any number: "))
-print(f"You typed the number {n}")
+try:
+    a = int(input("First number: "))
+    b = int(input("Second number: "))
+    r = a / b
 
-a = int(input("First number: "))
-b = int(input("Second number: "))
-r = a / b
-print(f"Dividing {a} by {b}, we have {r}")
+except (ValueError, TypeError):
+    print("We had a problem with the type of data that you typed.")
+
+except ZeroDivisionError:
+    print("You can't divide a number by zero.")
+
+except KeyboardInterrupt:
+    print("The user cancelled the program.")
+
+except Exception as error:
+    print(f"The error occurred: {error}")
+
+else:
+    print(f"Dividing {a} by {b}, we have {r}")
