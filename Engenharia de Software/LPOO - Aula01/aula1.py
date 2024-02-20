93f547eac866b4ea494a034ae49f515a4e4e4bec
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2024-02-20 10:58:25 -0300

    updating some files - initializing LPOO

---
+++ Engenharia de Software/LPOO - Aula01/aula1.py
@@ -0,0 +1,16 @@
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):
+    return n1 - n2
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+def divisao(n1, n2):
+    return n1 / n2
+
+print(soma(1, 1))
+print(subtracao(5, 3))
+print(multiplicacao(4, 3))
+print(divisao(10, 2))
