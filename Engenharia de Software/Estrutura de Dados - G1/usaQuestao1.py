871b1c59a5527ea32e5333a6431d18cab6a52da0
paulogosik <paulogosikmoita@gmail.com>
2024-05-02 19:25:12 -0300

    Creating files from G1

---
+++ Engenharia de Software/Estrutura de Dados - G1/usaQuestao1.py
@@ -0,0 +1,13 @@
+import Questao1
+
+lista = Questao1.Ldde()
+lista.inserir_fim(3)
+lista.inserir_fim(5)
+lista.inserir_fim(8)
+lista.inserir_fim(10)
+lista.inserir_fim(12)
+lista.show()
+lista.insere_em_ordem(1)
+lista.insere_em_ordem(9)
+lista.insere_em_ordem(13)
+lista.show()
---
+++ Engenharia de Software/Estrutura de Dados - G1/usaQuestao2.py
@@ -0,0 +1,9 @@
+import Questao2
+
+p = Questao2.pilha_dinamica()
+p.push("A")
+p.push("B")
+p.pop()
+p.push("C")
+print(p.getTopo())
+p.show()
